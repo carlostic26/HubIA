2c89f943566b8c1bc38341be9b8166e35ca482bb
carlostic26 <carlospenaranda26@gmail.com>
2024-03-06 10:00:44 -0500

    ad open app added

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,46 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:hubia/screens/screens_barril.dart';
 
+AppOpenAd? openAd;
+bool isAdLoaded = false;
+
+Future<void> loadOpenAd() async {
+  HubiaAdsIds ads = HubiaAdsIds();
+  try {
+    await AppOpenAd.load(
+      adUnitId: ads.openApp_adUnitId,
+      request: const AdRequest(),
+      adLoadCallback: AppOpenAdLoadCallback(
+        onAdLoaded: (ad) {
+          openAd = ad;
+          openAd!.show();
+          isAdLoaded = true;
+        },
+        onAdFailedToLoad: (error) {
+          isAdLoaded = false;
+        },
+      ),
+      orientation: AppOpenAd.orientationPortrait,
+    );
+  } catch (e) {
+    print('Error loading open ad: $e');
+    isAdLoaded = false;
+  }
+}
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await MobileAds.instance.initialize();
+
+  await loadOpenAd();
+
+  Timer(Duration(seconds: 10), () async {
+    if (!isAdLoaded) {
+      openAd?.dispose();
+    }
+  });
 
   runApp(const MyApp());
 }
